bbd64c02b0596273abe4e688f6193e7d0b0a3d04
Kim, Harim <harimkims@gmail.com>
2018-11-29 00:49:04 +0900

    db 에러 핸들링, 환경파일 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mysql = require('mysql');
+require('dotenv').config();
 
 const app = express();
 
@@ -17,14 +18,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-const connection = mysql.createConnection({
-  host: 'us-cdbr-iron-east-01.cleardb.net',
-  user: 'ba2cf57fd03e1d',
-  password: '1059b564',
-  database: 'heroku_fb274b19cae264c',
-});
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
 
-connection.connect();
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig);
+
+  connection.connect(function(err) {      // The server is either down
+    if (err) {                             // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 const jsonFormatter = (results = [], fields = []) => {
   return {
